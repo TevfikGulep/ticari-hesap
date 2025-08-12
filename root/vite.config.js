104fe33c09159c80a52bd289a9557857bd110a87
tevfikgulep <tevfikgulep@oxigen.team>
2025-08-12 15:00:11 +0000

    initial

--- root/vite.config.js
+++ root/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 9002,
+    hmr: {
+      protocol: 'wss',
+      host: '9000-firebase-hesaplayicigit-1754034725473.cluster-ombtxv25tbd6yrjpp3lukp6zhc.cloudworkstations.dev',
+      clientPort: 443, // Use 443 for WebSocket connections over HTTPS in the browser
+    },
+  },
 })
